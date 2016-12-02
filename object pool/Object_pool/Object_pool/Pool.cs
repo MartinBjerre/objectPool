9f4f3564d62200b71d131007b2b0281df7bd13ae
Martin <martinbjerre21@hotmail.com>
2016-12-02 11:37:23 +0100

    Adding Object Pool to repos.

---
+++ object pool/Object_pool/Object_pool/Pool.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Object_pool
+{
+    public class Pool
+    {
+        private static List<PoolObject> _available = new List<PoolObject>(); 
+        private  static List<PoolObject> _inUse = new List<PoolObject>();
+
+        public static PoolObject GetPoolObject()
+        {
+            lock (_available)
+            {
+                if (_available.Count != 0)
+                {
+                    PoolObject po = _available[0];
+                    _inUse.Add(po);
+                    _available.RemoveAt(0);
+                    return po;
+                }
+                else
+                {
+                    PoolObject po = new PoolObject();
+                    _inUse.Add(po);
+                    return po;
+                }
+            }
+        }
+
+        public static void ReleaseObject(PoolObject po)
+        {
+            CleanUp(po);
+            lock (_available)
+            {
+                _available.Add(po);
+                _inUse.Remove(po);
+            }
+        }
+
+        private static void CleanUp(PoolObject po)
+        {
+            po.TempData = null;
+        }
+    }
+}
---
+++ object pool/Object_pool/Object_pool/PoolObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Object_pool
+{
+    public class PoolObject
+    {
+        DateTime _CreateAt = DateTime.Now;
+
+        public DateTime CreateAt
+        {
+            get { return _CreateAt; }
+        }
+        public string TempData { get; set; }
+    }
+}
