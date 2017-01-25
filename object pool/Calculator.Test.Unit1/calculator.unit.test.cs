069dab963020029fdc7ce1850dad0eaa565887d6
MartinBjerre <Martinbjerre21@hotmail.com>
2017-01-25 10:50:18 +0100

    Adding test
    To test Travis

---
+++ object pool/Calculator.Test.Unit1/calculator.unit.test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using myCalculator;
+
+namespace Calculator.Test.Unit1
+{
+    [TestFixture]
+    public class calculatorTestUnit1
+    {
+        [Test]
+        public void add_add2and4_return6()
+        {
+            var uut = new myCalculatorClass();
+
+            Assert.That(uut.Add(2, 4), Is.EqualTo(6));
+        }
+
+        [Test]
+        public void add_add2andMinus4_returnMinus2()
+        {
+            var utt = new myCalculatorClass();
+            Assert.That(utt.Add(2,-4), Is.EqualTo(-2));
+        }
+
+    }
+}
---
+++ object pool/Calculator.Test.UnitTests/calculatorTestUnit1Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.Test.Unit1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Test.Unit1.Tests
+{
+    [TestClass()]
+    public class calculatorTestUnit1Tests
+    {
+        [TestMethod()]
+        public void add_add2and4_return6Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
